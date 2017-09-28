a28e7e64d054034b61284077bac9aa9bfc498303
Fernando Carmo <fernando_carmo500@hotmail.com>
2017-09-28 09:35:58 -0300

    almost done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Task } from './model/task';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: '<h1>Skeleton Project</h1>'
+    templateUrl: 'app.component.html',
+    styleUrls: [ 'app.component.css' ]
 })
 
-export class AppComponent { }
+export class AppComponent { 
+    private tasks = [
+        new Task("Buy a monkey", false),
+        new Task("Return the monkey", false),
+        new Task("do nothing", true),
+    ]
+    private currentTask = new Task(null, false);
+
+    addTask() {
+        let task = new Task(this.currentTask.content, this.currentTask.completed);
+        this.tasks.push(task);
+        this.currentTask.content = null;
+        this.currentTask.completed = null;
+    }
+}
